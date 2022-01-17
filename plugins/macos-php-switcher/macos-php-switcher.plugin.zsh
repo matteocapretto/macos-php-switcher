ed51a5b3a7f02ce593a56225c3752d592296d03c
Matteo Capretto <matteo.capretto@gmail.com>
2022-01-17 16:04:54 +0100

    brew plugin

---
+++ plugins/macos-php-switcher/macos-php-switcher.plugin.zsh
@@ -0,0 +1,12 @@
+switch-php() {
+    if [ -f .phpversion ]; then
+        targetVersion=$(<.phpversion)
+        currentVersion=$(php -v | grep -Eo '^PHP\s(\d{1,2}[.]{1}\d{1,2})')
+
+        if [ "PHP $targetVersion" != "$currentVersion" ]; then
+            echo "Switching to $targetVersion"
+            brew-php-switcher $targetVersion
+        fi
+    fi
+}
+add-zsh-hook chpwd switch-php
